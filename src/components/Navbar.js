c8a540a1dc398f70c0c2e864816c5cc0cdf83dda
Manuni Mistry <manunim.3011@gmail.com>
2024-12-13 18:21:05 +0530

    edited Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,11 +17,11 @@ const Navbar = () => {
       </button>
       <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
         <ul className='navul navli'>
-          <li><Link className="link navli" to="/product">Product</Link></li>
-          <li><Link className="link navli" to="/solutions">Solutions</Link></li>
-          <li><Link className="link navli" to="/resources">Resources</Link></li>
-          <li><Link className="link navli" to="/pricing">Pricing</Link></li>
-          <li className="btn2">Log in</li>
+          <li className="navli"><Link className="link" to="/product">Product</Link></li>
+          <li className="navli"><Link className="link" to="/solutions">Solutions</Link></li>
+          <li className="navli"><Link className="link" to="/resources">Resources</Link></li>
+          <li className="navli"><Link className="link" to="/pricing">Pricing</Link></li>
+          <li className="btn2 navli">Log in</li>
           <li className="btn2 link navli"><button className="navbtn">Get started now</button></li>
         </ul>
       </div>
